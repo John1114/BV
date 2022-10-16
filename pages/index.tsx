7d78abf2fc1c7b14fcda6e36ff9f5c457f840981
NuoWenLei <nuowen0612@gmail.com>
2022-10-15 23:51:25 -0400

    switch to next.js
    Currently it's just an App that says hello world.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+const TSComponent = () => {
+    return(
+      <div className="App">
+          <a href="/">With TS</a>
+          <br/>
+		  Hello World!
+      </div>
+    )
+  }
+
+  export default TSComponent
