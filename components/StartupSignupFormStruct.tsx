9da8ac262fe80e77c1cca1e8954353f75140c273
NuoWenLei <nuowen0612@gmail.com>
2022-10-22 23:06:56 -0400

    added page switch for forms and startup-signup pages

--- components/StartupSignupFormStruct.tsx
+++ components/StartupSignupFormStruct.tsx
@@ -2,21 +2,19 @@ export interface FormQuestion {
 	pageId: number;
 	question: string;
 	questionFormat: any;
-	nextPageFunction: any;
+	pageFunction: any;
   }
   
   export default function StartupFormStruct({
 	pageId,
 	question,
 	questionFormat,
-	nextPageFunction
+	pageFunction,
   }: FormQuestion) {
 	return (
-		<div id={pageId.toString()} className="relative rounded-lg overflow-hidden shadow-lg mx-auto h-5/6 w-5/6 bg-white">
+		<div id={pageId.toString()} className="relative rounded-lg overflow-hidden shadow-lg mx-auto h-5/6 w-4/6 bg-white">
 
 		<div className="bg-slate-100">
-
-
 			<div className="max-w-3xl pl-20 h-24 relative overflow-x-hidden">
 				<div className="absolute bottom-0">
 					<h1 className="text-3xl md:text-5xl font-bold text-slate-400">
@@ -27,23 +25,20 @@ export interface FormQuestion {
 
 
 		</div>
-		<div className="max-w-3xl pl-20 mt-6 pb-8">
-			<div className="flex justify-start items-center space-x-8 mb-8">
-				{questionFormat}
-			</div>
-		</div>
+		{questionFormat}
 
 		<div className="absolute right-0 bottom-0">
-		{/* <button type="button" className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 rounded-l">
-			Prev
-		</button> */}
-		<button type="button" onClick={nextPageFunction(pageId + 1)} className="bg-red-500 hover:bg-red-400 text-white font-bold py-6 px-16 rounded">
-			Next Page
-		</button>
-  </div>
-		{/* <div className="max-w-3xl relative overflow-x-hidden inline-flex">
-		
-		</div> */}
+			<button type="button" onClick={() => (pageFunction(pageId + 1))} className="bg-red-500 hover:bg-red-400 text-white font-bold py-6 px-16 rounded">
+				Next Page
+			</button>
+		</div>
+		<div className="absolute left-0 bottom-0">
+		{ (pageId > 0) ?
+			(<button type="button" onClick={() => (pageFunction(pageId - 1))} className="bg-gray-500 hover:bg-gray-400 text-white font-bold py-6 px-16 rounded">
+				Previous Page
+			</button>) : null
+		}
+		</div>
 	</div>
 	);
   }
--- pages/startup_signup.tsx
+++ pages/startup_signup.tsx
@@ -1,7 +1,73 @@
+import { EnergySavingsLeaf } from "@mui/icons-material";
 import { useState } from "react";
 import StartupFormStruct, { FormQuestion } from "../components/StartupSignupFormStruct"
 
 const questionPages: FormQuestion[] = [];
+
+
+questionPages.push(
+  {"pageId": 0, "question": "What is your company's name?", "pageFunction": null,
+"questionFormat": (
+  	<div className="max-w-full pl-20 pr-20 mt-6 pt-20 justify-center items-center">
+      <input required className="shadow appearance-none border rounded w-full h-24 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-4xl text-center" id="companyName" type="text" placeholder="Company Name" />
+		</div>
+)},
+{"pageId": 1, "question": "Tell us about your startup", "pageFunction": null,
+"questionFormat": (
+  	<div className="max-w-full pl-20 pr-20 mt-6 pt-10 justify-center items-center">
+      <div className="flex-col w-full">
+      <div className="flex justify-center items-center p-4">
+      <div className = "flex-col w-1/3 justify-center">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Year Founded
+        </label>
+        <input required className="shadow appearance-none border rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-sm" id="yearFounded" type="text" placeholder="Year Founded" />
+      </div>
+      <div className = "flex-col w-1/3 justify-center">
+      <div className="">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Founder(s)
+        </label>
+        <input required className="shadow appearance-none border rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-sm" id="founders" type="text" placeholder="Founder(s)" />
+        </div>
+      </div>
+      <div className = "flex-col w-1/3 justify-center">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Industry
+        </label>
+        <input required className="shadow appearance-none border rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-sm" id="yearFounded" type="text" placeholder="Select" />
+      </div>
+      </div>
+      <div className="flex justify-center p-4">
+      <div className = "flex-col w-full">
+      <label className="block text-gray-700 text-sm font-bold mb-2">
+        Description
+      </label>
+      <input required className="shadow appearance-none border rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-sm" id="description" type="text" placeholder="Description" />
+      </div>
+      </div>
+      </div>
+		</div>
+)},
+{"pageId": 2, "question": "Where can we find you?", "pageFunction": null,
+"questionFormat": (
+  	<div className="max-w-full pl-20 pr-20 mt-6 pt-20 justify-center items-center columns-2">
+       <div className="flex-col">
+       <label className="block text-gray-700 text-sm font-bold mb-2">
+        Email
+      </label>
+       <input required className="shadow appearance-none border rounded w-full h-16 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-2xl" id="email" type="text" placeholder="Email" />
+       
+       </div>
+       <div className="flex-col">
+       <label className="block text-gray-700 text-sm font-bold mb-2">
+        Website
+      </label>
+       <input required className="shadow appearance-none border rounded w-full h-16 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-2xl" id="website" type="text" placeholder="Website" />
+       </div>
+		</div>
+)},
+);
 
 // const questionPages: FormQuestion[] = [
 //   {
@@ -17,12 +83,22 @@ export default function StartupSignup() {
     // TODO: Add React useState for current pageId
     const [pageNumber, setPage] = useState<number>(0);
 
+    console.log(pageNumber);
 
-    return <div className="bg-form_background bg-[length:531px_631px] bg-no-repeat h-screen pt-40">
+    const toPage = async function(num: number){
+      if (num > questionPages[questionPages.length - 1].pageId){
+        // TODO: Submit form
+      }else if (num >= 0){
+        setPage(num)
+      }
+    }
+
+    return (<div className="bg-form_background bg-[length:531px_631px] bg-no-repeat h-screen pt-40">
       {questionPages.map((page: FormQuestion) => (
-
+        <div key={page.pageId.toString()} hidden={page.pageId != pageNumber} className="h-full">
+          <StartupFormStruct pageId={page.pageId} question={page.question} pageFunction={toPage} questionFormat={page.questionFormat}/>
+        </div>
         // TODO: Pass setState for page and hidden based on current pageId
-        <StartupFormStruct pageId={1} question="Tell us about your startup" nextPageFunction={setPage} questionFormat={<>some text <strong>bold text</strong> and more.</>}/>
       ))}
-    </div>
+    </div>)
 }
