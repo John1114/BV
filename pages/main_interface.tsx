fc3d1abcd7c11a991d77cd6a23ab23060742c344
Evan <evsammick@gmail.com>
2023-02-02 17:08:37 -0500

    Feb push

--- pages/main_interface.tsx
+++ pages/main_interface.tsx
@@ -7,7 +7,19 @@ import { Transition } from "@headlessui/react";
 import { CompanyData } from "../components/StartupInfoWithFirebase";
 import { fakeCompany1, fakeCompany2 } from '../src/mockData';
 import { height, minHeight } from "@mui/system";
+import Select from "react-select";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
+
+interface Option {
+    value: string,
+    label: string
+}
+
+
+let testDropdownOption: Option[] = [
+    {value: "test", label: "Test"},
+]
 
 export const interfaceBackgroundColor: string = '#EFEFEF'
 
@@ -67,18 +79,74 @@ export function SearchOptions() {
     const [showSidebar, setShowSidebar] = useState(false);
   
     return (
-        <div className="flex" style={{
+        <div className="flex h-full" style={{
             backgroundColor: interfaceBackgroundColor,
         }} >
                 <button onMouseOver={() => setShowSidebar(true)} onMouseLeave={() => setShowSidebar(false)}
-                    className={`w-24 hover:w-64`} >
+                    className={`w-24 hover:w-64 h-full`} >
                     <> 
                         {
                             showSidebar ? (
-                                <div>
+                                // https://www.npmjs.com/package/react-tabs?activeTab=readme thx
+                                <Tabs>
+                                    <div style={{ minHeight: '80%' }} className="w-full h-full align-middle">
+                                        <TabList className="bg-white object-top justify-center flex gap-4">
+                                            <Tab className="outline m-2 p-2 hover:bg-slate-400">People</Tab>
+                                            <Tab className="outline m-2 p-2 hover:bg-slate-400">Startups</Tab>
+                                        </TabList>
+                                
+                                        <div>
+                                            <div style={{ minHeight: '20rem' }}className=" w=8/12 bg-white rounded-lg py-4 px-4 content-center">
+                                                
+                                                {/* People panel (industry, role, expertise, affiliation, someone seeking role) */}
+                                                <TabPanel>
+                                                    <div className="content-center w-full my-5 align-middle">
+                                                        <b>Current Industry</b>
+                                                        <Select options={testDropdownOption} id="hi" isMulti key={"dropdown"}
+                                                            onChange={(value: any, _: any) => {}}/>
+                                                    </div>
+                                                    <div className="content-center w-full my-5 align-middle">
+                                                        <b>Role</b>
+                                                        <Select options={testDropdownOption} id="hi" isMulti key={"dropdown"}
+                                                            onChange={(value: any, _: any) => {}}/>
+                                                    </div>
+                                                    <div className="content-center w-full my-5 align-middle">
+                                                        <b>Expertise</b>
+                                                        <Select options={testDropdownOption} id="hi" isMulti key={"dropdown"}
+                                                            onChange={(value: any, _: any) => {}}/>
+                                                    </div>
+                                                    <div className="content-center w-full my-5 align-middle">
+                                                        <b>Affiliationy</b>
+                                                        <Select options={testDropdownOption} id="hi" isMulti key={"dropdown"}
+                                                            onChange={(value: any, _: any) => {}}/>
+                                                    </div>
+                                                </TabPanel>
+
+                                                {/* Startups panel (industry, needs, size) */}
+                                                <TabPanel>
+                                                    <div className="content-center w-full my-5 align-middle">
+                                                        <b>Industry</b>
+                                                        <Select options={testDropdownOption} id="hi" isMulti key={"dropdown"}
+                                                            onChange={(value: any, _: any) => {}}/>
+                                                    </div>
+                                                    <div className="content-center w-full my-5 align-middle">
+                                                        <b>Needs</b>
+                                                        <Select options={testDropdownOption} id="hi" isMulti key={"dropdown"}
+                                                            onChange={(value: any, _: any) => {}}/>
+                                                    </div>
+                                                    <div className="content-center w-full my-5 align-middle">
+                                                        <b>Size</b>
+                                                        <Select options={testDropdownOption} id="hi" isMulti key={"dropdown"}
+                                                            onChange={(value: any, _: any) => {}}/>
+                                                    </div>
+                                                </TabPanel>
+                                            
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </Tabs>
                                     
-                                    
-                                </div>
                             ) : (<div></div>)
                         }
                     </>
@@ -109,31 +177,31 @@ export function CompanyCard({data} : {data : CompanyData}) {
     return (
         <div style={{
             backgroundColor: data.accentColor,
-            minHeight: '18rem',
-            
 
                     }} 
 
         className=" w-9/12 rounded-md border-box shadow-lg">
             {/* Top */}
             <div className="flex justify-center">
-                <div className="my-2 mx-4 w-11/12">
+                <div className="my-2 mx-4 h-1/2 max-h-fit w-11/12">
                     <div className="flex justify-content items-center align-middle">
                         <img src={data.imageData} className="left-0 inset-y-0 w-24 h-24"/>
                         {/* Information */}
-                        <div className="mx-4">
-                            <h2 className="my-2 text-2xl">
-                                {data.name}
-                            </h2>
+                        <div className="mx-4 w-full">
+                            <div className="justify-content items-center">
+                                <h2 className="w-full my-2 text-2xl flex-wrap mx-1 overflow-wrap">
+                                    {data.name}
+                                </h2>
+                            </div>
                             {/* Bottom part of upper area */}
-                            <div className="grid-cols-3 gap-4 justify-content items-center flex content-between">
+                            <div className="grid-cols-3 gap-4 justify-content items-center flex flex-wrap content-between overflow-wrap">
                                 <div>
                                     <b>Industry</b>
                                     <p>{data.industry}</p>
                                 </div>
-                                <div>
+                                <div >
                                     <b>Founders</b>
-                                    <p>{data.founders}</p>
+                                    <p className="w-full flex-wrap mx-1">{data.founders}</p>
                                 </div>
                                 <div>
                                     <b>Contact</b>
@@ -151,7 +219,7 @@ export function CompanyCard({data} : {data : CompanyData}) {
             </div>
 
             {/* Bottom */}
-            <div className="mb-8 mt-2 flex justify-center w-full">
+            <div className="flex justify-center w-full h-full pb-10">
                 <div className="h-44 w-11/12 bg-white rounded-md">
                     {/* Text area */}
                     <p> 
