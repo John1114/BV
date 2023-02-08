4e72a163e9a93b3c12aef0a09dd312c291b6ee71
John Rytel <quantguideapp@gmail.com>
2023-02-08 18:25:02 -0500

    added user type

--- util/types.ts
+++ util/types.ts
@@ -1,6 +1,37 @@
+type Degree = {
+    type: string;
+    school: string;
+    concentration: string;
+    year: string;
+}
+
 export type User = {
     id: string;
     email: string;
-    name: string;
+    firstName: string;
+    lastName: string;
+    residence: string;
+    education: Degree[];
     profilePicture: string;
+    resume: string;
+    linkedin: string;
+    websites: string[];
+    type: string[];
+    addtionalInfo: string;
+    industry: string;
+    companyWebsite: string;
+    jobsInfo: string;
+    myStartup: string;
+    expertise: string[];
+    expertiseAddional: string;
+    lookingFor: string;
+    lookingForAdditional: string;
+    seekingExpertise: string[];
+    seekingExpertiseAdditional: string;
+    reasonContacted: string[];
+    reasonContactedAdditional: string;
+    brownAffilitation: string[];
+    brownAffilitationOther: string[];
+
+
 }
